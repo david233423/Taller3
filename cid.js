5c2f9d49ad90d32bc1551bd68369e9dcfbb2e68d
David Alvear <alveardavid23@gmail.com>
2023-05-22 15:42:37 -0500

    Version1

---
+++ cid.js
@@ -0,0 +1,15 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+const offcanvasElementList = document.querySelectorAll('.offcanvas')
+const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
+
+const carousel = new bootstrap.Carousel('#myCarousel')
